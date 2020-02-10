b1b0e9fae2c216da58821fefa9f125730f57d000
shuvrag <49293791+shuvrag@users.noreply.github.com>
2020-02-10 11:13:16 -0500

    Update breathe-easy.py

--- breathe-easy.py
+++ breathe-easy.py
@@ -13,8 +13,6 @@ st.write('The particulate matter and weather forecast in Kolkata for the next 12
 
 fig = go.Figure()
 
-fig = go.Figure()
-
 # Add scatter trace for line
 fig.add_trace(go.Scatter(
     x=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
@@ -61,6 +59,8 @@ fig.update_layout(
 
 st.write(fig)
 #fig.show()
+
+fig = go.Figure()
 
 # Add scatter trace for line
 fig.add_trace(go.Scatter(
